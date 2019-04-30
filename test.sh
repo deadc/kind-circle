7d4c5ceebc1e112dac7edbc642b13b30385243dc
deadc <deadcow@archlinux.com.br>
2019-04-30 03:37:16 -0300

    test

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+attempt_counter=0
+max_attempts=2
+check=0
+
+while [ ${check} -lt 3 ] ; do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo -e "\nMax attempts reached"
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    check=$( kubectl get pods --namespace platform --field-selector=status.phase=Running 2> /dev/null | grep -i httpbin | wc -l )
+    sleep 5
+done
